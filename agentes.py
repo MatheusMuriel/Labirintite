b74c660c71d8cf2d83be32d62e271aa3854bc80d
Matheus Muriel <matheus.muriel@outlook.com>
2019-05-14 13:03:41 -0300

    definido mecanismo de agentes

--- agentes.py
+++ agentes.py
@@ -2,6 +2,8 @@
 
 from abc import ABC, abstractmethod
 
+
+
 class Agente(ABC):
     '''
     Classe abstrata de agentes artificiais racionais.
@@ -24,7 +26,32 @@ class Agente(ABC):
 # Implemente seu jogador humano nessa classe, sobrescrevendo os métodos
 # abstratos de Agente. Em construir_agente, retorne uma instância dessa classe.
 class AgenteHumano(Agente):
+
+    sequencia = 0
+    agentes = []
     
+    def __init__(self):
+        
+        #Define aqui o ID do agente
+        self.__class__.sequencia += 1
+        self.id = self.__class__.sequencia
+
+        #Define aqui o tipo do agente
+        self.tipo_agente = 'HUMANO'
+        
+        #Adiciona esse agente na lista de agentes
+        self.__class__.agentes.append(self)
+
+    def __str__(self):
+        return self.tipo_agente
+
+    def __repr__(self):
+        return '<{}: {} - {}\n'.format(self.__class__.__name__, self.id, self.tipo_agente)
+
+    @classmethod
+    def all(cls):
+        return cls.agentes
+
     def adquirirPercepcao(self, percepcao_mundo):
         # Utilize percepcao de mundo para atualizar tela (terminal ou blit),
         # tocar sons, dispositivos hápticos, etc, todo e qualquer dispositivo
@@ -36,7 +63,46 @@ class AgenteHumano(Agente):
         # Receba entrada humana apenas neste momento, seja com prompt (terminal)
         # ou polling (jogos interativos).
         
-        raise input('Proxima ação: ')
+        return input('Proxima ação: ')
+
+class AgenteRobo(Agente):
+    sequencia = 0
+    agentes = []
+    
+    def __init__(self):
+        
+        #Define aqui o ID do agente
+        self.__class__.sequencia += 1
+        self.id = self.__class__.sequencia
+
+        #Define aqui o tipo do agente
+        self.tipo_agente = 'ROBO'
+        
+        #Adiciona esse agente na lista de agentes
+        self.__class__.agentes.append(self)
+
+    def __str__(self):
+        return self.tipo_agente
+
+    def __repr__(self):
+        return '<{}: {} - {}\n'.format(self.__class__.__name__, self.id, self.tipo_agente)
+
+    @classmethod
+    def all(cls):
+        return cls.agentes
+
+    def adquirirPercepcao(self, percepcao_mundo):
+        # Utilize percepcao de mundo para atualizar tela (terminal ou blit),
+        # tocar sons, dispositivos hápticos, etc, todo e qualquer dispositivo
+        # de saída para interface humana.
+        
+        print("Ainda não implementado")
+    
+    def escolherProximaAcao(self):
+        # Receba entrada humana apenas neste momento, seja com prompt (terminal)
+        # ou polling (jogos interativos).
+        
+        print("Ainda não implementado")
 
 
 def construir_agente(*args,**kwargs):
